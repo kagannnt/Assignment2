866a3a9a3fb18e6e098dda2d03f5f67d2e1f64cb
kagannnt <tasbolattkagan@gmail.com>
2025-12-23 11:17:10 +0500

    Update Yoga.java

--- src/Yoga.java
+++ src/Yoga.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class Yoga extends Activity {
+    private String type;
 
-public class Yoga {
+    public Yoga(int duration, String type) {
+        super("Yoga", duration);
+        this.type = type;
+    }
+
+    @Override
+    public double calculateCalories() {
+        return getDuration() * 4.0;
+    }
 }
