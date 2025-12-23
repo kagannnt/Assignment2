aca4a0c68a6e1bc8f73459ea68830ef26b54f57c
kagannnt <tasbolattkagan@gmail.com>
2025-12-23 11:17:48 +0500

    Update Activity.java

--- src/Activity.java
+++ src/Activity.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
-public class Activity {
+public abstract class Activity {
+    private String name;
+    private int duration;
+
+    public Activity(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public String getName() { return name; }
+    public int getDuration() { return duration; }
+
+
+    public abstract double calculateCalories();
+
+
+    @Override
+    public String toString() {
+        return "Activity: " + name + ", Duration: " + duration + " min, Calories: " + calculateCalories();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Activity activity = (Activity) o;
+        return duration == activity.duration && Objects.equals(name, activity.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, duration);
+    }
 }
